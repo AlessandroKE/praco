40b502810b65ac48f0f601edfe934a2ff12c7ed5
SandroKE <koomealessandro@gmail.com>
2023-05-26 14:45:21 +0300

    updates

---
+++ Includes/article-form.php
@@ -0,0 +1,28 @@
+<?php if (! empty($errors)) : ?>
+    <ul>
+        <?php foreach ($errors as $error) : ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+<form method="post">
+
+    <div>
+        <label for="title">Title</label>
+        <input name="title" id="title" placeholder="Article title" value="<?= htmlspecialchars($title); ?>">
+    </div>
+
+    <div>
+        <label for="content">Content</label>
+        <textarea name="content" rows="4" cols="40" id="content" placeholder="Article content"><?= htmlspecialchars($content); ?></textarea>
+    </div>
+
+    <div>
+        <label for="published_at">Publication date and time</label>
+        <input type="datetime-local" name="published_at" id="published_at" value="<?= htmlspecialchars($published_at); ?>">
+    </div>
+
+    <button>Save</button>
+
+</form>
---
+++ edit-article.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'includes/config.php';
+require 'includes/article.php';
+
+//$conn = getDB();
+
+if (isset($_GET['id'])) {
+
+    $article = getArticle($conn, $_GET['id']);
+
+    if ($article) {
+
+        $title = $article['title'];
+        $content = $article['content'];
+        $published_at = $article['published_at'];
+
+    } else {
+
+        die("article not found");
+
+    }
+
+} else {
+
+    die("id not supplied, article not found");
+}
+
+?>
+<?php //require 'includes/header.php'; ?>
+
+<h2>Edit article</h2>
+
+<?php require 'includes/article-form.php'; ?>
+
+<?php require 'includes/footer.php'; ?>
