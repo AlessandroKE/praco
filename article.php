31f12e696fc8d639ee3eababb720aa68f58cc7c6
SandroKE <koomealessandro@gmail.com>
2023-06-03 21:17:11 +0300

    updates

--- article.php
+++ article.php
@@ -25,7 +25,10 @@ if (isset($_GET['id'])) {
         <h2><?= htmlspecialchars($article['title']); ?></h2>
         <p><?= htmlspecialchars($article['content']); ?></p>
     </article>
+
 <a href = "edit-article.php?id=<?=$article['Id'];?>">Edit</a>
+<a href = "delete-article.php?id=<?=$article['Id'];?>">Delete</a>
+
 <?php endif; ?>
 
 <?php require 'includes/footer.php'; ?>
