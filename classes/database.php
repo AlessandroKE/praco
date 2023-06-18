f20b9341f72553e546bea16a7998dc9522aa9e18
SandroKE <koomealessandro@gmail.com>
2023-06-18 13:34:53 +0300

    updating errror handling methods

--- classes/database.php
+++ classes/database.php
@@ -3,15 +3,25 @@ class database {
     public $host = "localhost";
     public $user = "root";
     public $password = "";
-    public $db_name = "cms";
+    public $db_name = "cm";
 
 
     //{$this} is a special variable that refers to the current instance of the class. 
     //It is used within class methods to access properties and methods of the class.
+
+    //PDO uses  object oriented programming for handling errors (exceptions)
     
     public function getConn()
     {
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';charset=utf8';
+
+        //Checking for errors using try and catch method
+        try{
         return new PDO($dsn, $this->user, $this->password);
+        }catch(PDOException $e){
+
+            echo "Error: " . $e->getMessage();
+
+        }
     }
 }
