801bfd367c2a7479fed8d9a874ea3faf74c189a0
SandroKE <koomealessandro@gmail.com>
2023-06-21 00:34:21 +0300

    updated

--- classes/Article.php
+++ classes/Article.php
@@ -1,42 +1,78 @@
 <?php
 
-Class Article{
+/**
+ * Article
+ *
+ * A piece of writing for publication
+ */
+class Article
+{
+    /**
+     * Unique identifier
+     * @var integer
+     */
+    public $id;
 
+    /**
+     * The article title
+     * @var string
+     */
+    public $title;
 
-   public static function getArticle($conn) {
-    $sql = "SELECT *
-        FROM article
-        ORDER BY published_at;";
+    /**
+     * The article content
+     * @var string
+     */
+    public $content;
 
-$results = $conn->query($sql);
+    /**
+     * The publication date and time
+     * @var datetime
+     */
+    public $published_at;
 
-if ($results === false) {
-  $conn->errorInfo();
-} else {
-    //$articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
-    //Fectching the results as an associative array
-    return $results->fetchAll(PDO::FETCH_ASSOC);
+    /**
+     * Get all the articles
+     *
+     * @param object $conn Connection to the database
+     *
+     * @return array An associative array of all the article records
+     */
+    public static function getArticle($conn)
+    {
+        $sql = "SELECT *
+                FROM article
+                ORDER BY published_at;";
 
-}
-   }
+        $results = $conn->query($sql);
 
-   public static function getById($conn, $id) {
-    $sql = "SELECT * FROM article WHERE Id = :id";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-
-    //Fectching the properties of the class as an object.
-
-    $stmt->setFetchMode(PDO::FETCH_CLASS, 'Article');
-
-    if ($stmt->execute()) {
-        //Fetching the properties of the class as an array.
-        //return $stmt->fetch(PDO::FETCH_ASSOC);
+        return $results->fetchAll(PDO::FETCH_ASSOC);
     }
-    // Add error handling or return a default value if needed
-    return null;
-}
 
-}
+    /**
+     * Get the article record based on the ID
+     *
+     * @param object $conn Connection to the database
+     * @param integer $id the article ID
+     * @param string $columns Optional list of columns for the select, defaults to *
+     *
+     * @return mixed An object of this class, or null if not found
+     */
+    public static function getByID($conn, $id, $columns = '*')
+    {
+        $sql = "SELECT $columns
+                FROM article
+                WHERE Id = :id";
 
-?>
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Article');
+
+        if ($stmt->execute()) {
+
+            return $stmt->fetch();
+
+        }
+    }
+}
